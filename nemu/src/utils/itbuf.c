dc627e939a46f6e800f8398a8766501caf40b535
tracer-ysyx <tracer@ysyx.org>
2026-03-05 11:10:52 +0800

    > compile NEMU ysyx wangzexue Linux wang-VMware-Virtual-Platform 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:10:52 up 4 days, 11:15,  1 user,  load average: 0.91, 1.39, 1.08

--- nemu/src/utils/itbuf.c
+++ nemu/src/utils/itbuf.c
@@ -53,10 +53,10 @@ void itbuf_d(int n)
     for (int i = 0; i < n - 1; i++) {
         int idx = (start + i) % IT_LEN;
         printf("    ");
-        printf("IT[%03d] PC=" FMT_WORD "  %s\n", idx, my_itbuf[idx].pc, my_itbuf[idx].logstr);
+        printf("IT[%03d]   %s\n", idx,  my_itbuf[idx].logstr);
     }
 
         printf(" => ");
-        printf("IT[%03d] PC=" FMT_WORD "  %s\n", it_pos-1, my_itbuf[it_pos - 1].pc, my_itbuf[it_pos - 1].logstr);
+        printf("IT[%03d]   %s\n", it_pos-1,  my_itbuf[it_pos - 1].logstr);
 
 }
