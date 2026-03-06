6f2a5fbe671be015d8a8a53996158a95ef70b2b0
tracer-ysyx <tracer@ysyx.org>
2026-03-06 11:17:45 +0800

    > compile NEMU ysyx wangzexue Linux wang-VMware-Virtual-Platform 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:17:45 up 5 days,  3:48,  1 user,  load average: 4.42, 2.46, 1.63

---
+++ nemu/src/utils/mtrace.c
@@ -0,0 +1,15 @@
+#include "mtrace.h"
+#include <memory/paddr.h>
+
+void p_mread(paddr_t addr, int len, word_t val) //物理内存读
+{
+    printf("mem read at " FMT_PADDR " len = %d val = %d\n", addr, len, val);
+    log_write("MTR R " FMT_PADDR " len=%d val = %d\n", addr, len, val);
+}
+
+
+void p_mwrite(paddr_t addr, int len, word_t data)
+{
+    printf("mem write at " FMT_PADDR " len = %d data = %08x\n",addr, len, data);
+    log_write("MTR W " FMT_PADDR " len=%d val=" FMT_WORD "\n", addr, len, data);
+}
