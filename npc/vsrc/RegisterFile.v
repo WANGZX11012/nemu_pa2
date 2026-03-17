518054c2ba5bacc87447789618701accbd9fd4df
tracer-ysyx <tracer@ysyx.org>
2026-03-17 20:59:19 +0800

    > sim RTL ysyx wangzexue Linux wang-VMware-Virtual-Platform 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:59:19 up 2 days, 15:40,  1 user,  load average: 1.43, 1.26, 1.32

--- npc/vsrc/RegisterFile.v
+++ npc/vsrc/RegisterFile.v
@@ -1,12 +1,27 @@
-module RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
+module RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (//二读一写寄存器堆 必须有clk
   input clk,
-  input [DATA_WIDTH-1:0] wdata,
-  input [ADDR_WIDTH-1:0] waddr,
-  input wen
+  input [DATA_WIDTH-1:0] w_data,
+  input [ADDR_WIDTH-1:0] w_addr,
+
+  input [ADDR_WIDTH-1:0] r_addr1,
+  input [ADDR_WIDTH-1:0] r_addr2,
+
+  input wen,
+
+  output [DATA_WIDTH-1:0]r_data1,
+  output [DATA_WIDTH-1:0]r_data2
+
 );
-  reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];  //2**代表幂运算
+  reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];  //2**代表幂运算 也就是rf 31
+
+
   always @(posedge clk) begin
-    if (wen && waddr != 0) rf[waddr] <= wdata;
+    if (wen && w_addr != 0) rf[w_addr] <= w_data;
     
   end
+
+  assign r_data1 = (r_addr1 == 0) ? 0 : rf[r_addr1];
+  assign r_data2 = (r_addr2 == 0) ? 0 : rf[r_addr2];
+
 endmodule
+
