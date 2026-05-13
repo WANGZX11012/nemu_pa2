c3a030b72e61beff9778def4ae4c48d419b0ae52
tracer-ysyx <tracer@ysyx.org>
2026-05-13 21:19:13 +0800

    > compile NEMU ysyx wangzexue Linux wang-VMware-Virtual-Platform 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:19:13 up 6 days, 14:18,  1 user,  load average: 0.42, 0.42, 0.44

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -41,7 +41,8 @@ static uint32_t *vgactl_port_base = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 
-static void init_screen() {
+static void init_screen() 
+{
   SDL_Window *window = NULL;
   char title[128];
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
@@ -95,4 +96,5 @@ void init_vga() {
   add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
   IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
+  IFDEF(CONFIG_VGA_SHOW_SCREEN, update_screen());
 }
